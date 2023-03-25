47a8be9adda1f11d00c67762a705aa1fcb25f4e7
Fabien KORGO <kograss20@gmail.com>
2023-03-25 17:23:31 +0000

    Adding socket io on port 7777

--- src/main/java/EchoEndpoint.java
+++ src/main/java/EchoEndpoint.java
@@ -1,9 +1,21 @@
+import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 @ServerEndpoint("/echo")
 public class EchoEndpoint {
+
+    @OnOpen
+    public void onOpen(Session session) throws IOException {
+        ServerSocket ss = new ServerSocket(7777);
+        System.out.println("Awaiting connexion");
+        session.getBasicRemote().sendText("Awaiting connexion");
+        ss.close();
+    }
     @OnMessage
     public void onMessage(Session session, String message)
     {
