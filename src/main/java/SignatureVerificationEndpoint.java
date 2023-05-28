c49d97e2f911a8ba903fedd228f72ffcae13e834
Fabien KORGO <kograss20@gmail.com>
2023-05-28 23:33:53 +0100

    Signature done

--- src/main/java/SignatureVerificationEndpoint.java
+++ src/main/java/SignatureVerificationEndpoint.java
@@ -7,7 +7,9 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.Signature;
@@ -16,7 +18,7 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.concurrent.CountDownLatch;
 
-@ServerEndpoint("/verify")
+@ServerEndpoint("/signature")
 public class SignatureVerificationEndpoint {
 
     private static Gson gson = new Gson();
@@ -29,24 +31,33 @@ public class SignatureVerificationEndpoint {
     @OnOpen
     public void onOpen(Session session) throws IOException{
         session.setMaxIdleTimeout(1000 * 60 * 60);
+        session.setMaxTextMessageBufferSize(20000000);
+        session.setMaxBinaryMessageBufferSize(20000000);
         //latch = new CountDownLatch(1);
     }
 
-    public static boolean verifySignature(byte[] claimBytes, byte[] signatureBytes, X509Certificate certificate) throws Exception {
+    public static boolean verifySignature(byte[] claimBytes, byte[] signatureBytes, byte[] certificateBytes) throws Exception {
+        ByteArrayInputStream bais = new ByteArrayInputStream(certificateBytes);
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        X509Certificate x509Certificate = (X509Certificate) ois.readObject();
         // Create a Signature object and initialize it with the public key from the certificate
         Signature verifier = Signature.getInstance("SHA256withRSA");
-        verifier.initVerify(certificate);
+        verifier.initVerify(x509Certificate);
         verifier.update(claimBytes);
 
         return verifier.verify(signatureBytes);
     }
-    @OnMessage
-    public void onMessage(String verificationParametersJson, Session session) throws InterruptedException, IOException {
-
+    @OnMessage(maxMessageSize = 20000000)
+    public void onMessage(String verificationParametersJson, Session session) throws Exception {
+        System.out.println("JSON "+verificationParametersJson);
         SignatureVerificationParameters verificationParameters = gson.fromJson(verificationParametersJson, SignatureVerificationParameters.class);
-        boolean response = verifySignature(verificationParameters.vcEncryptedData, verificationParameters.signatureBytes, );
-        System.out.println();
-        session.getBasicRemote().sendText(gson.toJson(user));
+        boolean response = verifySignature(verificationParameters.vcEncryptedData, verificationParameters.signatureBytes, verificationParameters.certificateBytes);
+        if(response){
+            session.getBasicRemote().sendText(VerificationEndpoint.cppServerUrl);
+        }else{
+            session.getBasicRemote().sendText("");
+        }
+
 
     }
 }
