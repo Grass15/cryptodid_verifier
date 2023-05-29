d3aa745230c613965b6690eebb741587d614b336
Fabien KORGO <kograss20@gmail.com>
2023-05-29 22:44:53 +0100

    Blockchain API

--- src/main/java/SignatureVerificationEndpoint.java
+++ src/main/java/SignatureVerificationEndpoint.java
@@ -1,22 +1,19 @@
 import com.google.gson.Gson;
-import com.loginid.cryptodid.WebsocketClientEndpoint;
-import com.loginid.cryptodid.model.Claim;
 import com.loginid.cryptodid.model.SignatureVerificationParameters;
 
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.Signature;
 import java.security.cert.X509Certificate;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.concurrent.CountDownLatch;
+import java.util.Arrays;
+
+import okhttp3.*;
+
 
 @ServerEndpoint("/signature")
 public class SignatureVerificationEndpoint {
@@ -33,7 +30,17 @@ public class SignatureVerificationEndpoint {
         //latch = new CountDownLatch(1);
     }
 
-    public static boolean verifySignature(byte[] claimBytes, byte[] signatureBytes, byte[] certificateBytes) throws Exception {
+    public String isVCInRevocationTree(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+
+    public boolean verifySignature(byte[] claimBytes, byte[] signatureBytes, byte[] certificateBytes, byte[] revocationNonce, int version) throws Exception {
         ByteArrayInputStream bais = new ByteArrayInputStream(certificateBytes);
         ObjectInputStream ois = new ObjectInputStream(bais);
         X509Certificate x509Certificate = (X509Certificate) ois.readObject();
@@ -41,14 +48,19 @@ public class SignatureVerificationEndpoint {
         Signature verifier = Signature.getInstance("SHA256withRSA");
         verifier.initVerify(x509Certificate);
         verifier.update(claimBytes);
-
-        return verifier.verify(signatureBytes);
+        if(verifier.verify(signatureBytes)){
+            String url = "http://192.168.11.105:3000/isvcinRevocationsMerkleTree?revNonce="+ Arrays.toString(revocationNonce) +"&version="+version+"\"";
+            return !Boolean.parseBoolean(this.isVCInRevocationTree(url));
+        }
+        else {
+            return false;
+        }
     }
     @OnMessage(maxMessageSize = 20000000)
     public void onMessage(String verificationParametersJson, Session session) throws Exception {
         System.out.println("JSON "+verificationParametersJson);
         SignatureVerificationParameters verificationParameters = gson.fromJson(verificationParametersJson, SignatureVerificationParameters.class);
-        boolean response = verifySignature(verificationParameters.vcEncryptedData, verificationParameters.signatureBytes, verificationParameters.certificateBytes);
+        boolean response = verifySignature(verificationParameters.vcEncryptedData, verificationParameters.signatureBytes, verificationParameters.certificateBytes, verificationParameters.revocationNonce, verificationParameters.version);
         if(response){
             session.getBasicRemote().sendText(VerificationEndpoint.cppServerUrl);
         }else{
--- src/main/java/com/loginid/cryptodid/model/SignatureVerificationParameters.java
+++ src/main/java/com/loginid/cryptodid/model/SignatureVerificationParameters.java
@@ -7,10 +7,15 @@ public class SignatureVerificationParameters implements Serializable {
     public byte[] vcEncryptedData;
     public byte[] signatureBytes;
     public byte[] certificateBytes;
+    public byte[] revocationNonce;
+    public int version;
 
-    public SignatureVerificationParameters(byte[] vcEncryptedData, byte[] signatureBytes, byte[] certificateBytes) {
+    public SignatureVerificationParameters(byte[] vcEncryptedData, byte[] signatureBytes, byte[] certificateBytes, byte[] revocationNonce, int version) {
         this.vcEncryptedData = vcEncryptedData;
         this.signatureBytes= signatureBytes;
         this.certificateBytes = certificateBytes;
+        this.revocationNonce = revocationNonce;
+        this.version = version;
     }
+
 }
